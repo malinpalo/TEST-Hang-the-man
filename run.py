b9883cd6c55212b965adade5cd803d31ee59ed81
malinpalo <malinpalo.mp@gmail.com>
2022-12-26 21:52:51 +0000

    Create a welcome area

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random # generates random options
+from words import hidden_words # imports hidden_words list from word.py
+
+print("Welcome to the game of hang the man! ")
+print("------------------------------------")
+print("Rules to apply")
+print("------------------------------------")
